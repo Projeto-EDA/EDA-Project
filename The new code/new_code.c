9e43b96330d29f51160bb7de83c2a5b82893010f
joaovitorml <joao.lemos4000@gmail.com>
2017-10-13 01:50:16 -0300

    Saindo da jaula o monstro

--- The new code/new_code.c
+++ The new code/new_code.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+void flush_in() {
+    int ch;
+    do {
+        ch = fgetc(stdin);
+    } while (ch != EOF && ch != '\n');
+}
 
 struct elemento {
   char band;
   char nome;
+  int tempo;
   int pos;
   struct elemento *ant;
   struct elemento *prox;
@@ -34,6 +41,7 @@ Menu *cria_lista(Menu *l,int tam){
       Elemento *novo = (Elemento *)malloc(sizeof(Elemento));
       novo->band = 'H';
       novo->nome = '*';
+      novo->tempo = 0;
       novo->pos = i;
       if(l->tam == 0){
         l->inicio = novo;
@@ -59,30 +67,102 @@ void printar(Menu *l){
     return;
   }
   Elemento *no = l->inicio;
-  printf("Nó %d\n",no->pos);
-  printf("%c\n",no->band);
+  printf("Nó %d:",no->pos);
+  printf("%c - ",no->band);
   printf("%c\n",no->nome);
   no = no->prox;
   while(no != l->inicio){
-    printf("Nó %d\n",no->pos);
-    printf("%c\n",no->band);
+    printf("Nó %d:",no->pos);
+    printf("%c - ",no->band);
     printf("%c\n",no->nome);
     no = no->prox;
   }
 }
 
+Menu *altera_lista(Menu *l,char name, int p,int temp){
+  Elemento *aux;
+  aux = l->inicio;
+  int cont=0;
+
+  while(cont<p){
+    if(aux->band == 'H'){
+    aux->band = 'P';
+    aux->nome = name;
+    aux->tempo = temp;
+    aux = aux->prox;
+    cont++;
+    }
+    else{
+      aux = aux->prox;
+    }
+  }
+
+  return (l);
+}
+
+int remover(Menu *l,int tam,int temp){
+  Elemento *no;
+  no = l->inicio;
+  if(no->tempo <=temp && no->band == 'P'){
+    no->band = 'H';
+    no->nome = '*';
+    tam++;
+  }
+  no = no->prox;
+  while(no != l->inicio){
+    if(no->tempo <=temp && no->band == 'P'){
+      no->band = 'H';
+      no->nome = '*';
+      tam++;
+    }
+    no = no->prox;
+  }
+  return tam;
+}
+
 
 int main(){
   Menu *l;
-  int tam;
-  printf("Digite o tamanho da memória: ");
+  int tam,tam2,temp,opcao,Chronos=0;
+  char name;
+  printf("\nDigite o tamanho da memória: ");
   scanf("%d",&tam);
   l = lst_cria();
   l = cria_lista(l,tam);
 
-  printar(l);
+  do{
+  printf("\n0-Sair\n");
+  printf("1-Passar o tempo\n");
+  printf("2-Adicionar processo\n");
+  printf("3-Printar memória\n\n");
+  printf("Escolha uma opcao: ");
+  scanf("%d",&opcao);
+  if(opcao==1){
+    Chronos++;
+    tam = remover(l,tam,Chronos);
+  }
+  if(opcao==2){
+    flush_in();
+    printf("Informe o nome do processo: ");
+    scanf("%c",&name);
+    flush_in();
+    printf("Informe o tamanho do processo: ");
+    scanf("%d",&tam2);
+    printf("Informe o tempo de duração: ");
+    scanf("%d",&temp);
+    if(tam >= tam2){
+      l = altera_lista(l,name,tam2,temp);
+      tam = tam - tam2;
+    }
+    else{
+      printf("\nMemória insuficiente\n");
+    }
+}
+  if(opcao==3){
+    printar(l);
+  }
 
-
+}while(opcao!=0);
 
   return 0;
 }
